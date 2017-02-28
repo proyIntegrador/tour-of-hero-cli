d2594d976d9dea1edf68d07b679d6a3f54bbe0b8
andrec230 <andrec230@gmail.com>
2017-02-27 20:08:01 -0500

    Commit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  heroes = HEROES;  
+  selectedHero: Hero;
   hero: Hero = {
     id:1,
     name: 'Windstorm'
   };
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }  
 }
 export class Hero {
   id: number;
   name: string;
 }
+const HEROES: Hero[] = [
+  {id: 11, name: 'Mr. Nice'},
+  {id: 12, name: 'JackJack'},
+  {id: 13, name: 'Mr. Bombastic'},
+  {id: 14, name: 'Magneta'},
+  {id: 15, name: 'DeadPool'},
+  {id: 16, name: 'Dynamo'},
+  {id: 17, name: 'Gambito'},
+  {id: 18, name: 'Buzz'},
+  {id: 19, name: 'Batman'},
+  {id: 20, name: 'Woody'}
+];
