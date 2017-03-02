e92d4e98b311792e8c7fb09eed20c2a26492c31a
andrec230 <andrec230@gmail.com>
2017-03-01 23:07:08 -0500

    Commit 5 parcial Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,11 @@
 import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
-
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
-
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [HeroService]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    providers: []
 })
-export class AppComponent implements OnInit {
-  title = 'Tour of Heroes by Andrew';
-  heroes: Hero[];  
-  selectedHero: Hero; 
-
-  constructor(private heroService: HeroService) {}
-
-  getHeroes(): void{
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes) ;
-  }
-
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-  }  
-
-  ngOnInit():void {
-    this.getHeroes();
-  }
+export class AppComponent {
+    title = 'Tour of Heroes by Andrew';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { HeroDetailComponent } from './heroes/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroService } from './heroes/hero.service';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [ HeroService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
